dd1009d5159a1653c7d13fc94e8627b3ca0024c7
Sarah Allen <sarah@zooniverse.org>
2021-03-18 11:15:08 -0500

    Update colors

--- packages/lib-classifier/src/plugins/tasks/SurveyTask/components/components/Chooser/components/CharacteristicsFilter/FilterStatus/FilterStatus.js
+++ packages/lib-classifier/src/plugins/tasks/SurveyTask/components/components/Chooser/components/CharacteristicsFilter/FilterStatus/FilterStatus.js
@@ -19,7 +19,7 @@ const StyledDropButton = styled(DropButton)`
   padding: 5px 10px;
 
   ${props => props.backgroundColor ? css`
-    background-color: ${props.theme.global.colors['accent-2']};
+    background-color: ${props.theme.global.colors['accent-1']};
   ` : css`
     background-color: none;
   `}
@@ -87,7 +87,7 @@ export default function FilterStatus (props) {
         icon={<Filter />}
         label={
           <StyledLabel 
-            color='neutral-2'
+            color='neutral-1'
           >
             {counterpart('SurveyTask.filter')}
           </StyledLabel>
